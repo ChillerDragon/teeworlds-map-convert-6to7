988ceb66a0cbecefe75c0df0f53d60780fa5e85f
ChillerDragon <ChillerDragon@gmail.com>
2023-12-10 12:51:00 +0100

    C compiler styled warning cli flags

--- twmap_6to7.py
+++ twmap_6to7.py
@@ -4,6 +4,7 @@ import os.path
 import sys
 import json
 import argparse
+from typing import Optional
 
 import numpy
 import twmap # type: ignore
@@ -14,19 +15,45 @@ EXAMPLE_TEXT = '''example:
 all_args = argparse.ArgumentParser(
                                  epilog=EXAMPLE_TEXT,
                                  formatter_class=argparse.RawDescriptionHelpFormatter)
-all_args.add_argument('-v', '--verbose',  help='verbose output')
-all_args.add_argument('-s', '--strict',  help='fail instead of best effort translation on edge case doodads')
+all_args.add_argument('-v', '--verbose',  help='verbose output', action = 'store_true')
+all_args.add_argument('-s', '--strict',  help='fail instead of best effort translation on edge case doodads', action = 'store_true')
+all_args.add_argument('-Werror', '--Werror',  help='abort with error on warning', action = 'store_true')
+all_args.add_argument('-Wempty', '--Wempty',  help='warn if empty tiles are used', action = 'store_true')
+all_args.add_argument('-Wmapping', '--Wmapping',  help='show warnings for problematic tiles from the mappings file', action = 'store_true', default = True)
+all_args.add_argument('-Wno-mapping', '--Wno-mapping',  help='turn off -Wmapping', action = 'store_true')
+all_args.add_argument('-Weverything', '--Weverything',  help='turn every warning on', action = 'store_true')
+all_args.add_argument('-Wall', '--Wall',  help='turn all sensible warnings on', action = 'store_true')
 all_args.add_argument('INPUT_MAP')
 all_args.add_argument('OUTPUT_MAP')
 
 args = vars(all_args.parse_args())
 
-def dbg(msg):
+def dbg(msg: str) -> None:
     if not args['verbose']:
         return
     print(msg)
 
-def replace_doodads(layer, mapping):
+def warn(warn_type: str, msg: str) -> None:
+    if not is_warn(warn_type):
+        return
+    print(f"Warning: {msg} [-{warn_type}]")
+    if args['Werror']:
+        print('Error on waring because -Werror is on')
+        sys.exit(1)
+
+def is_warn(warn: str) -> bool:
+    if args['Weverything']:
+        return True
+    if args['Wall']:
+        if warn != 'Wempty':
+            return True
+    off = f"Wno-{warn[1:]}"
+    if off in args:
+        if args[off]:
+            return False
+    return args[warn]
+
+def replace_doodads(layer, mapping: dict) -> None:
     progress = 0
     edited_tiles = layer.tiles
     for (y, x, flags), tile in numpy.ndenumerate(layer.tiles):
@@ -35,23 +62,26 @@ def replace_doodads(layer, mapping):
             print(x, y)
         if flags == 0:
             if str(tile) in mapping:
-                warn = f"{tile}_warn"
-                if warn in mapping:
-                    print(f"Warning: {mapping[warn]}")
-                    if args['strict']:
-                        print('abort on warning because strict mode is active')
-                        sys.exit(1)
-                edited_tiles[y][x][flags] = mapping[str(tile)]
+                warn_key = f"{tile}_warn"
+                if warn_key in mapping:
+                    warn('Wmapping', mapping[warn])
+                mapped = mapping[str(tile)]
+                if mapped == 0:
+                    warn('Wempty', f"Empty tile used at x={x} y={y}")
+                edited_tiles[y][x][flags] = mapped
     layer.tiles = edited_tiles
 
-def get_mapping(image_name, direction):
+def get_mapping(image_name, direction) -> Optional[dict]:
     mapping_path = f"./mappings/{direction}_{image_name}.json"
     if os.path.isfile(mapping_path):
         with open(mapping_path, encoding='utf-8') as f:
             return json.load(f)
     return None
 
-def main():
+def main() -> None:
+    if args['Werror']:
+        print('All warnings will be errors')
+    dbg(f"translating map {args['INPUT_MAP']}")
     m = twmap.Map(args['INPUT_MAP'])
     for group in m.groups:
         for layer in group.layers:
